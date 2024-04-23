b6efc947ec7ea91cf18964acc4d857ed4ed9a687
George Manuell Irizarry <mojomojoke9@gmail.com>
2024-04-22 22:06:25 -0400

    completed labs

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,13 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return ( <div id="home">
+            Home
+            <h1 style={color}>{name} is a Web Developer from {city}</h1>
+          </div>
+  )
 }
-
+const color = {
+  color: 'firebrick'
+}
 export default Home;
